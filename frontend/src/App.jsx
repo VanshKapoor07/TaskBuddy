e9411b3925cc31e7f6b5ab69b1858322315ecc1b
Vansh Kapoor <vanshkapoor0703@gmail.com>
2025-03-26 15:58:21 +0530

    navbar started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/Button'
+import Header from './components/Header'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+   
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,13 +21,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Router>
+      <Header />
+      </Router>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Button text={`Count is ${count}`} onClick={() => setCount((count) => count + 1)} className={"cursor-not-allowed opacity-50"}/>
+        <button text="okkk">
+      Hello
+      </button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Button = ({ text, onClick, className = "" }) => {
+  console.log(className);
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 rounded-md text-white bg-blue-500 hover:bg-blue-600 transition ${className}`}
     >
       {text}
     </button>
